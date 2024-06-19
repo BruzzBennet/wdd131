dd40ebd75b6b83b53aedf5f541b7c398a911ce2b
BruzzBennet <bruce.bennet.trevino@gmail.com>
2024-06-19 01:50:26 -0600

    templeactivity

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+const title = document.querySelector('#title');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+    title.classList.toggle('show');
+});
